e14322b7332ff45a768fbfd951dbb0305ffad5eb
Christopher Knapp <chris.knapp@live.de>
2023-04-23 21:32:10 +0200

    remove storyboard and add first simple example AR object

--- CaboAR/ViewController.swift
+++ CaboAR/ViewController.swift
@@ -6,19 +6,80 @@
 //
 
 import UIKit
+import ARKit
 import RealityKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var arView: ARView!
+    private var arView: ARView = ARView(frame: .zero)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(arView)
+        arView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            arView.topAnchor.constraint(equalTo: view.topAnchor),
+            arView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            arView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            arView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        ])
+        arView.session.delegate = self
+        arView.addCoaching()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        arView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:))))
+    }
+    // MARK: - Object Placement
+    
+    func placeObject(named entityName: String, for anchor: ARAnchor) {
+        let entity = try! ModelEntity.loadModel(named: entityName)
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        entity.generateCollisionShapes(recursive: true)
+        arView.installGestures([.rotation, .translation], for: entity)
+        
+        let anchorEntity = AnchorEntity(anchor: anchor)
+        anchorEntity.addChild(entity)
+        arView.scene.addAnchor(anchorEntity)
+    }
+    
+    // MARK: - Touch Interaction
+    
+    @objc func handleTap(recognizer: UITapGestureRecognizer) {
+        let location = recognizer.location(in: arView)
+        
+        let results = arView.raycast(from: location, allowing: .estimatedPlane, alignment: .horizontal)
+        if let firstResult = results.first {
+            let anchor = ARAnchor(name: "robot_walk_idle", transform: firstResult.worldTransform)
+            arView.session.add(anchor: anchor)
+        } else {
+            print("Object placement failed. Couldn't find a surface.")
+        }
+    }
+}
+
+// MARK: - ARView Coaching Overlay
+extension ARView: ARCoachingOverlayViewDelegate {
+    func addCoaching() {
+        let coachingOverlay = ARCoachingOverlayView()
+        coachingOverlay.delegate = self
+        coachingOverlay.session = self.session
+        coachingOverlay.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        coachingOverlay.layer.position = CGPoint(x: self.bounds.size.width/2, y: self.bounds.size.height/2)
+        coachingOverlay.goal = .anyPlane
+        self.addSubview(coachingOverlay)
+    }
+    
+    public func coachingOverlayViewDidDeactivate(_ coachingOverlayView: ARCoachingOverlayView) {
+        //Ready to add entities next?
+    }
+}
+
+// MARK: - ARSessionDelegate
+extension ViewController: ARSessionDelegate {
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        for anchor in anchors {
+            if let anchorName = anchor.name, anchorName == "robot_walk_idle" {
+                placeObject(named: anchorName, for: anchor)
+            }
+        }
     }
 }
